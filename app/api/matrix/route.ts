832d2365d6a24a8f57884931efd5f6d0202e4685
Bruno Scachetti <brunoscachetti@hotmail.com>
2025-01-09 10:42:55 -0300

    Added Error logging to Matrix route

--- app/api/matrix/route.ts
+++ app/api/matrix/route.ts
@@ -13,41 +13,49 @@ interface MatrixCSVType {
 }
 
 export async function GET(req: NextRequest) {
-  const filePath = "utils/EXEMPLO MATRIZ.csv";
-  const address = req.nextUrl.searchParams.get("address") || null;
-  const id = req.nextUrl.searchParams.get("id") || null;
-  //   const fileContent = fs
-  //     .readFileSync(filePath, { encoding: "utf-8" })
-  //     .split("\r\n")
-  //     .slice(1)
-  //     .map((line) => line.split(","));
-  //   const matrixData: MatrixDataType[] = fileContent.map((row) => ({
-  //     id: row[0],
-  //     address: row[1],
-  //     coordinates: row[2] + ", " + row[3],
-  //     type: row[4],
-  //   }));
-  const fileContent = fs.readFileSync(filePath, { encoding: "utf-8" });
-  const parsedContent = Papa.parse(fileContent, {
-    header: true,
-  });
-  let matrixData: MatrixDataType[] = (
-    parsedContent.data as MatrixCSVType[]
-  ).map((row) => ({
-    id: row.id,
-    address: row.name,
-    coordinates: row.latitude + ", " + row.longitude,
-    type: row.kind,
-    media: row.media,
-  }));
-  if (address) {
-    matrixData = matrixData.filter((row) =>
-      row.address.toLowerCase().includes(address.toLowerCase())
+  try {
+    const filePath = "utils/EXEMPLO MATRIZ.csv";
+    const address = req.nextUrl.searchParams.get("address") || null;
+    const id = req.nextUrl.searchParams.get("id") || null;
+    //   const fileContent = fs
+    //     .readFileSync(filePath, { encoding: "utf-8" })
+    //     .split("\r\n")
+    //     .slice(1)
+    //     .map((line) => line.split(","));
+    //   const matrixData: MatrixDataType[] = fileContent.map((row) => ({
+    //     id: row[0],
+    //     address: row[1],
+    //     coordinates: row[2] + ", " + row[3],
+    //     type: row[4],
+    //   }));
+    const fileContent = fs.readFileSync(filePath, { encoding: "utf-8" });
+    const parsedContent = Papa.parse(fileContent, {
+      header: true,
+    });
+    let matrixData: MatrixDataType[] = (
+      parsedContent.data as MatrixCSVType[]
+    ).map((row) => ({
+      id: row.id,
+      address: row.name,
+      coordinates: row.latitude + ", " + row.longitude,
+      type: row.kind,
+      media: row.media,
+    }));
+    if (address) {
+      matrixData = matrixData.filter((row) =>
+        row.address.toLowerCase().includes(address.toLowerCase())
+      );
+    }
+    if (id) {
+      // matrixData = matrixData.filter((row) => row.id.includes(id));
+      matrixData = matrixData.filter((row) => row.id === id);
+    }
+    return NextResponse.json(matrixData);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: `Internal Server Error: ${error}` },
+      { status: 500 }
     );
   }
-  if (id) {
-    // matrixData = matrixData.filter((row) => row.id.includes(id));
-    matrixData = matrixData.filter((row) => row.id === id);
-  }
-  return NextResponse.json(matrixData);
 }
