94f18dd9005f7a58d789928ee5f67f1ff571ad89
Bruno Scachetti <brunoscachetti@hotmail.com>
2025-01-09 16:54:54 -0300

    Cleanup

--- utils/authOptions.ts
+++ utils/authOptions.ts
@@ -6,15 +6,11 @@
 // import prisma from '@/lib/prisma';
 
 import { NextAuthOptions } from "next-auth";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-// import prisma from "./prisma";
-import { Adapter } from "next-auth/adapters";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcrypt";
 import { query } from "./mysqlConnection";
 import { CadGeral } from "@/types/databaseTypes";
 export const authOptions: NextAuthOptions = {
-  // adapter: PrismaAdapter(prisma) as Adapter,
   secret: process.env.NEXTAUTH_SECRET,
   providers: [
     CredentialsProvider({
@@ -25,24 +21,6 @@ export const authOptions: NextAuthOptions = {
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials) {
-        // await prisma.$connect();
-        // const user = await prisma.clientes.findUnique({
-        //   where: {
-        //     email: credentials ? credentials.email : "",
-        //   },
-        // });
-        // if (!user) {
-        //   throw new Error("Usuário não encontrado");
-        // }
-        // const isPasswordCorrect = await bcrypt.compare(
-        //   credentials?.password!,
-        //   user.password!
-        // );
-        // if (!isPasswordCorrect) {
-        //   throw new Error("Senha incorreta");
-        // }
-        // return user as any;
-
         const userFetch = await query(
           "SELECT * FROM cadgeral WHERE email = ? AND Cliente = 1",
           [credentials?.email]
@@ -68,9 +46,6 @@ export const authOptions: NextAuthOptions = {
   session: {
     strategy: "jwt",
   },
-  //   pages: {
-  //     signIn: "/login",
-  //   },
   callbacks: {
     async jwt({ token, user, session, trigger }) {
       if (trigger === "update") {
@@ -92,68 +67,3 @@ export const authOptions: NextAuthOptions = {
     },
   },
 };
-
-// export const authOptions: NextAuthOptions = {
-//   adapter: PrismaAdapter(prisma) as Adapter,
-//   providers: [
-//     CredentialsProvider({
-//       id: 'credentials',
-//       name: 'Credentials',
-//       credentials: {
-//         login: { label: 'Login', type: 'text' },
-//         password: { label: 'Password', type: 'password' },
-//       },
-//       async authorize(credentials) {
-//         await prisma.$connect();
-//         const user = await prisma.funcionarios.findUnique({
-//           where: {
-//             login: credentials ? credentials.login : '',
-//           },
-//         });
-//         if (!user) {
-//           throw new Error('Usuário não cadastrado');
-//         }
-//         if (!user.ativo) {
-//           throw new Error('Usuário desligado, obrigado pela contribuição!');
-//         }
-//         const isPasswordCorrect =
-//           (await bcrypt.compare(credentials?.password!, user.senha!)) ||
-//           (credentials?.password === user.senha && user.senha.length < 60);
-//         if (!isPasswordCorrect) {
-//           throw new Error('Senha incorreta');
-//         }
-//         const treatedUser = {
-//           ...user,
-//         };
-//         return treatedUser as any;
-//       },
-//     }),
-//   ],
-//   session: {
-//     strategy: 'jwt',
-//   },
-//   secret: process.env.NEXTAUTH_SECRET,
-//   pages: {
-//     signIn: '/',
-//   },
-//   callbacks: {
-//     async jwt({ token, user, session, trigger }) {
-//       if (trigger === 'update') {
-//         const updatedUser = {
-//           ...token,
-//           user: {
-//             ...session,
-//           },
-//         };
-//         user = updatedUser as any;
-//         return { user };
-//       }
-//       user && (token.user = user);
-//       return token;
-//     },
-//     async session({ session, token }) {
-//       session = token.user as any;
-//       return session;
-//     },
-//   },
-// };
