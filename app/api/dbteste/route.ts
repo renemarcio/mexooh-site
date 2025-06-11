f7a02f7d4f29c8c4d32a625e3285c37607a62d33
MEX <rene@marciorene.eng.br>
2025-06-11 09:15:05 -0300

    ajustes build

---
+++ app/api/dbteste/route.ts
@@ -0,0 +1,12 @@
+// app/api/dbtest/route.ts
+import { query } from "@/utils/mysqlConnection";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const result = await query("SELECT 1 + 1 AS result");
+    return NextResponse.json({ success: true, result });
+  } catch (err) {
+    return NextResponse.json({ success: false, error: String(err) }, { status: 500 });
+  }
+}
